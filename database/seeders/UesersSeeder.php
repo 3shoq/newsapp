ac71637055b6e87defb6f766929404201580cb82
@3shoq <eng.bamlhes@gmail.com>
2021-07-03 00:18:48 +0300

    after seeds

---
+++ database/seeders/UesersSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+
+class UesersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //User::factory()->times(30)->create();
+    }
+}
